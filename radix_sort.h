8e7063822b1784b477527fa3b8dbbcf361715b49
Payam Siyari <payamsiyari@gmail.com>
2016-03-25 18:30:58 -0400

    G,P,L,T fixed.
    G,P,L,T fixed.
    Working on AlltoAllV...

--- radix_sort.h
+++ radix_sort.h
@@ -16,6 +16,8 @@
 #define GET_DIGIT(key, k, offset) (((key) >> (offset)) & ((1 << (k)) - 1))
 
 #define TEST_CODE 1
+template <typename T>
+void counting_sort(T* begin, T* end, unsigned int (*key_func)(const T&), unsigned int k = 16, unsigned int d=0);
 /**
  * @brief   Parallel distributed radix sort.
  *
@@ -114,51 +116,168 @@ void radix_sort(T* begin, T* end, unsigned int (*key_func)(const T&), MPI_Dataty
 
         std::vector<unsigned int> G(num_buckets, 0);
         MPI_Allreduce(&hist.front(), &G.front(), hist.size(), MPI_UNSIGNED, MPI_SUM, comm);
-//        std::cout << "Global Histogram" << "\n";
-//        for (std::vector<unsigned int>::iterator iter = G.begin() ; iter != G.end() ; iter++){
-//            std::cout << *iter << ", ";
-//        }
-//        std::cout << "\n";
+        for (std::vector<unsigned int>::iterator iter = G.begin() + 1; iter != G.end() ; iter++){
+            *iter += *(iter-1);
+        }
+        G.insert(G.begin(),0);
+        G.pop_back();
 
+#if TEST_CODE
+        std::cout << "G Histogram" << rank << "\n";
+        for (std::vector<unsigned int>::iterator iter = G.begin() ; iter != G.end() ; iter++){
+            std::cout << *iter << ", ";
+        }
+        std::cout << "\n";
+#endif
         MPI_Barrier (comm);
 
         std::vector<unsigned int> P(num_buckets, 0);
         MPI_Exscan(&hist.front(), &P.front(), hist.size(), MPI_UNSIGNED, MPI_SUM, comm);
-//        std::cout << "P Histogram" << "\n";
-//        for (std::vector<unsigned int>::iterator iter = P.begin() ; iter != P.end() ; iter++){
-//            std::cout << *iter << ", ";
-//        }
-//        std::cout << "\n";
+        std::cout << "P Histogram" << rank << "\n";
+        for (std::vector<unsigned int>::iterator iter = P.begin() ; iter != P.end() ; iter++){
+            std::cout << *iter << ", ";
+        }
+        std::cout << "\n";
 
         MPI_Barrier (comm);
 
         std::vector<unsigned int> L(np, 0);
         int L_index = 0;
         for (T* iter = begin; iter < end ; iter++, L_index++){
-            L[L_index] = L_index - L_backup[key_func(*iter)];
+            L[L_index] = L_index - L_backup[GET_DIGIT(key_func(*iter), k, d)];
         }
-//        std::cout << "L Histogram" << "\n";
-//        for (std::vector<unsigned int>::iterator iter = L.begin() ; iter != L.end() ; iter++){
-//            std::cout << *iter << ", ";
-//        }
-//        std::cout << "\n";
-        std::vector<unsigned int> T(L);
+        std::cout << "L Histogram" << rank << "\n";
+        for (std::vector<unsigned int>::iterator iter = L.begin() ; iter != L.end() ; iter++){
+            std::cout << *iter << ", ";
+        }
+        std::cout << "\n";
+        std::vector<unsigned int> Tarray(L);
         int T_index = 0;
         for (T* iter = begin; iter < end ; iter++, T_index++){
-            T[T_index] += G[GET_DIGIT(key_func(*iter), k, d)] + P[GET_DIGIT(key_func(*iter), k, d)];
+            Tarray[T_index] += G[GET_DIGIT(key_func(*iter), k, d)] + P[GET_DIGIT(key_func(*iter), k, d)];
         }
-
+        std::cout << "Tarray" << rank << "\n";
+        for (std::vector<unsigned int>::iterator iter = Tarray.begin() ; iter != Tarray.end() ; iter++){
+            std::cout << *iter << ", ";
+        }
+        std::cout << "\n";
         MPI_Barrier (comm);
 
+        //Calculating send_counts and send_displacements
+        std::vector<unsigned int> send_counts(p,0);
+        std::vector<unsigned int> send_displacements(p,0);
+        std::vector<unsigned int> TarrayDividedByLocalSize(Tarray);//For Testing send_counts and send_displacements
+        T_index = 1;
+        send_counts[*(Tarray.begin())/np]++;
+        TarrayDividedByLocalSize[0] /= np;//For testing
+        for (std::vector<unsigned int>::iterator iter = Tarray.begin()+1 ; iter != Tarray.end() ; iter++, T_index++){
+            int destination_processor_rank = *iter/np;
+            TarrayDividedByLocalSize[T_index] /= np;//For testing
+            int previous_destination_processor_rank = *(iter-1)/np;
+            send_counts[destination_processor_rank]++;
+            if(destination_processor_rank != previous_destination_processor_rank){
+                send_displacements[destination_processor_rank] = T_index;
+            }
+        }
+        std::cout << "TarrayDividedByLocalSize" << "\n";
+        for (std::vector<unsigned int>::iterator iter = TarrayDividedByLocalSize.begin() ; iter != TarrayDividedByLocalSize.end() ; iter++){
+            std::cout << *iter << ", ";
+        }
+        std::cout << "\n";
+        std::cout << "send_counts" << "\n";
+        for (std::vector<unsigned int>::iterator iter = send_counts.begin() ; iter != send_counts.end() ; iter++){
+            std::cout << *iter << ", ";
+        }
+        std::cout << "\n";
+        std::cout << "send_displacements" << "\n";
+        for (std::vector<unsigned int>::iterator iter = send_displacements.begin() ; iter != send_displacements.end() ; iter++){
+            std::cout << *iter << ", ";
+        }
+        std::cout << "\n";
 
+        //Calculating recv_counts and recv_displacements
+        std::vector<unsigned int> recv_counts(p,0);
+        MPI_Alltoall(&send_counts.front(), p-1, MPI_UNSIGNED, &recv_counts.front(), p-1, MPI_UNSIGNED, comm);
+        std::cout << "recv_counts" << "\n";
+        for (std::vector<unsigned int>::iterator iter = recv_counts.begin() ; iter != recv_counts.end() ; iter++){
+            std::cout << *iter << ", ";
+        }
+        std::cout << "\n";
+
+
+        // Compute Receive displacements
+        std::vector<unsigned int> recv_disp(p,0);
+        for (std::vector<unsigned int>::iterator iter = recv_disp.begin()+1; iter != recv_disp.end(); iter++) {
+            *iter += *(iter-1);
+        }
+
+        std::cout << "recv_disp" << "\n";
+        for (std::vector<unsigned int>::iterator iter = recv_disp.begin() ; iter != recv_disp.end() ; iter++){
+            std::cout << *iter << ", ";
+        }
+        std::cout << "\n";
+
+        std::vector<T> recv_buff(begin, end); // how to initialize
+        MPI_Alltoallv(reinterpret_cast<void*>(result.data()), reinterpret_cast<const int*>(send_counts.data()), reinterpret_cast<const int*>(send_displacements.data()), dt, reinterpret_cast<void*>(recv_buff.data()),
+                      reinterpret_cast<const int*>(recv_counts.data()), reinterpret_cast<const int*>(recv_disp.data()), dt, comm);
+
+        std::cout << "recv_buffer" << "\n";
+
+
+        size_t offset = np*sizeof(T);
+        for( T* recv_iter = recv_buff.data(); recv_iter < recv_buff.data() + offset;  recv_iter++) {
+            std::cout << key_func(*recv_iter) << ", ";
+        }
+        std::cout << std::endl;
+
+
+//        size_t offset = np*sizeof(T);
+        // Locally sort receive buffer using counting sort.
+//        counting_sort(recv_buff.data(), recv_buff.data() + offset,key_func,k,d);
+//
+//        // Copy these values to the begin and end
+//        int index = 0;
+//        T* recv_iter = recv_buff.data();
+//        for(T* iter = begin; iter != end; iter++, recv_iter++) {
+//            *iter = *recv_iter;
+//        }
         // TODO:
         // 1.) create histogram and sort via bucketing (~ counting sort) DONE
         // 2.) get global histograms (P, G) via MPI_Exscan/MPI_Allreduce,... DONE (sorta!)
-        // 3.) calculate send_counts
+        // 3.) calculate send_counts locally
         // 4.) communicate send_counts to get recv_counts
         // 4.) calculate displacements
         // 6.) MPI_Alltoallv
         // 7.) local sorting via bucketing (~ counting sort)
+    }
+}
+
+template <typename T>
+void counting_sort(T* begin, T* end, unsigned int (*key_func)(const T&), unsigned int k = 16, unsigned int d=0) {
+    size_t np = end - begin;
+    unsigned int num_buckets = 1 << k;
+    std::vector<T> result(np);//Temporary sorted vector by the key field
+    std::vector<unsigned int> hist(num_buckets, 0);
+    for (T* iter = begin; iter < end ; iter++){//Calculating the histogram
+        hist[GET_DIGIT(key_func(*iter), k, d)]++;
+    }
+
+    std::vector<unsigned int> sum_hist(np, 0);//Will store the cumulative values
+    //Indicates the starting index of each bucket
+    for (std::vector<unsigned int>::iterator sum_hist_index = sum_hist.begin() + 1, hist_index = hist.begin() + 1; sum_hist_index != sum_hist.end() ; sum_hist_index++, hist_index++) {
+        *sum_hist_index += *(sum_hist_index-1) + *(hist_index-1);
+    }
+    std::vector<unsigned int> L_backup(sum_hist);//sum_hist for calculating L
+
+    for (T* iter = begin; iter < end ; iter++){//Peforming the sorting
+        result[sum_hist[GET_DIGIT(key_func(*iter), k, d)]] = *iter;
+        sum_hist[GET_DIGIT(key_func(*iter), k, d)]++;
+    }
+    int result_index = 0;
+
+    /* Copy the sorted values to the original array */
+    for (T* iter = begin; iter < end ; iter++, result_index++){
+        *iter = result[result_index];
     }
 }
 //void arraySum(void* invec, void* inoutvec, int *len, MPI_Datatype *datatype){
